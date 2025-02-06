f111c9da5bbff8300bf98f9b010206237c4a5666
Adzkar Fauzie <adzkarfauzie02@gmail.com>
2025-02-06 17:49:06 +0700

    Releasing v0.1.1

---
+++ TableBody.d.ts
@@ -0,0 +1,2 @@
+import { InitRecordType, TableBodyProps } from "./type";
+export declare const TableBody: <RecordType extends InitRecordType>(props: TableBodyProps<RecordType>) => import("react/jsx-runtime").JSX.Element;
