02c90ed92069b978d3e90dabee5c9fdbcd85fcb3
Adzkar Fauzie <adzkarfauzie02@gmail.com>
2025-02-06 17:52:32 +0700

    Releasing v0.1.2

---
+++ TableHeader.d.ts
@@ -0,0 +1,2 @@
+import { InitRecordType, TableHeaderProps } from "./type";
+export declare const TableHeader: <RecordType extends InitRecordType>(props: TableHeaderProps<RecordType>) => import("react/jsx-runtime").JSX.Element;
