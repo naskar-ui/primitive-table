19ad45f5474b394ab15867d3ee82eddb9a81b5e7
Adzkar Fauzie <adzkarfauzie02@gmail.com>
2025-02-11 10:17:54 +0700

    Releasing v0.1.5

---
+++ Table.d.ts
@@ -0,0 +1,2 @@
+import { InitRecordType, TableProps } from "./type";
+export declare const Table: <RecordType extends InitRecordType>(props: TableProps<RecordType>) => import("react/jsx-runtime").JSX.Element;
